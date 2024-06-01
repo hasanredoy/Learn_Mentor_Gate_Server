d92c15a0cfa07f7d2d4ec77faa8f07e27543917f
unknown <redoyhasan4585@gmail.com>
2024-06-01 15:09:01 +0400

    added mongodb setup and create partners api

--- Server-Side-Repo(LMG)/index.js
+++ Server-Side-Repo(LMG)/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const jsonWebToken = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const PORT = process.env.PORT || 5000
 app.use(express.json())
@@ -10,6 +11,38 @@ app.use(cors({
   origin:['http://localhost:5173']
 }))
 app.use(cookieParser)
+
+
+
+
+const uri =`mongodb+srv://${process.env.Db_user}:${process.env.Db_pass}@cluster01.2xfw1xu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+      const partnersCollections = client.db('Learn-Mentor-GateDB').collection('partners')
+      
+      app.get('/partners',async(req,res)=>{
+        const result = await partnersCollections.find().toArray()
+        res.send(result)
+      })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/',(req,res)=>{
    res.send('Learn Monitor Gate Server is Running')
