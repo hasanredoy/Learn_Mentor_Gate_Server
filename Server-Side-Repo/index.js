8aec4a1b49a5e31f47d49c27485605be351cedd7
unknown <redoyhasan4585@gmail.com>
2024-06-10 14:44:29 +0400

    make some changes

--- Server-Side-Repo/index.js
+++ Server-Side-Repo/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173",'https://learn-mentor-gate.firebaseapp.com','https://learn-mentor-gate.web.app'],
+    origin: ['https://learn-mentor-gate.firebaseapp.com','https://learn-mentor-gate.web.app'],
   })
 );
 app.use(cookieParser());
